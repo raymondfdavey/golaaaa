5b42b672165e1602ee3fed01ca16ba57d6376396
raymondfdavey <raymondfdavey@gmail.com>
2020-04-18 15:22:25 +0100

    Updates

---
+++ precache-manifest.15d1966da20e16eed96734634f149599.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d826a39790d7ab38407b6276c54c187c",
+    "url": "/golaaaa/index.html"
+  },
+  {
+    "revision": "8044f89d567c76b69622",
+    "url": "/golaaaa/static/css/main.7183ccec.chunk.css"
+  },
+  {
+    "revision": "776cc187ed4b0db38845",
+    "url": "/golaaaa/static/js/2.41bdc456.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/golaaaa/static/js/2.41bdc456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8044f89d567c76b69622",
+    "url": "/golaaaa/static/js/main.6cb88050.chunk.js"
+  },
+  {
+    "revision": "acbe3c354966d3a23bad",
+    "url": "/golaaaa/static/js/runtime-main.f9390dd4.js"
+  },
+  {
+    "revision": "a2120ce1d3a5081ff95ba20d6a0481c5",
+    "url": "/golaaaa/static/media/Hearts.a2120ce1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/golaaa/precache-manifest.0c76c9ffed33cc4b10ed8d26df56b394.js"
+  "/golaaaa/precache-manifest.15d1966da20e16eed96734634f149599.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/golaaa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/golaaaa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
