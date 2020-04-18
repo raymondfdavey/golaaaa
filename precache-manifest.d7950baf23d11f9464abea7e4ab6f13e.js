49024576578bb06000ffbfcafbac65a520fe9c74
raymondfdavey <raymondfdavey@gmail.com>
2020-04-18 15:03:54 +0100

    Updates

---
+++ precache-manifest.d7950baf23d11f9464abea7e4ab6f13e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff3e17710d12379fab820292ea79847",
+    "url": "/golaaa/index.html"
+  },
+  {
+    "revision": "bd45d9354587995857a8",
+    "url": "/golaaa/static/css/main.6a803cdf.chunk.css"
+  },
+  {
+    "revision": "776cc187ed4b0db38845",
+    "url": "/golaaa/static/js/2.41bdc456.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/golaaa/static/js/2.41bdc456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd45d9354587995857a8",
+    "url": "/golaaa/static/js/main.91f9b91f.chunk.js"
+  },
+  {
+    "revision": "b65347757c0b248c5344",
+    "url": "/golaaa/static/js/runtime-main.fcae30ce.js"
+  },
+  {
+    "revision": "a2120ce1d3a5081ff95ba20d6a0481c5",
+    "url": "/golaaa/static/media/Hearts.a2120ce1.jpg"
+  }
+]);
